692ab4511756764ad30bf095d850c728ef9ac8d5
bhalisasodo <bhalisasodo10@gmail.com>
2026-07-08 22:09:17 +0200

    Hardcode base path to '/' in vite config to force root paths

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-const basePath = process.env.VITE_BASE_PATH || '/';
 
 export default defineConfig({
-    base: basePath,
+    base: '/',
     plugins: [
         react(),
         tailwindcss(),
